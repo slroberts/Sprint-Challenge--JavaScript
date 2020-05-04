afa080efb1aae637f2f6f36f257a0518c734be3a
slroberts <shomariroberts@gmail.comclear>
2020-05-04 00:22:17 -0400

    Updated Request 3

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -144,8 +144,9 @@ const contactInfo = graduates.map((student) => {
 console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
-const unisWithUni = graduates.filter((hasUni) =>
-  hasUni.university.includes("Uni")
+
+const unisWithUni = graduates.filter(
+  (hasUni) => hasUni.university.indexOf("Uni") > -1
 );
 console.log(unisWithUni);
 
